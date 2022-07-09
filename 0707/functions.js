a17440993b0620c9184c037a34c2bb12c193c21b
ShokoLocoMocco <stachikawa@gradcenter.cuny.edu>
2022-07-08 20:15:52 -0400

    Mac

---
+++ 0707/functions.js
@@ -0,0 +1,37 @@
+let x = 2;
+let y = 3;
+
+//let multiply = function(){
+//    console.log(x*y)
+//}
+
+//function multiply(){
+//    console.log(x*y)
+//}
+
+//let multiply = () =>{
+//    console.log(x*y)
+//}
+
+//function multiply(first, second){
+//    console.log(first*second)
+//}
+
+function multiply(first, second){
+    product = (first*second);
+}
+
+multiply(x, y)
+
+console.log(product)
+
+
+let movieArray = ["Indiana Jones", "Ghostbusters", "Star Wars", 4, 5, 6, ["Inception"]];
+
+console.log(movieArray)
+
+movieArray.push("Breakin'")
+
+console.log(movieArray)
+
+
