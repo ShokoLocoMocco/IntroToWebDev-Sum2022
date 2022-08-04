94c2fc65aa7eabd7db9f79ea724e7a2256c20bd5
ShokoLocoMocco <stachikawa@gradcenter.cuny.edu>
2022-08-04 12:12:58 -0400

    dropdown

--- asset/script.js
+++ asset/script.js
@@ -1,32 +1,41 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://twitter60.p.rapidapi.com/user_info?user_name=yua_mikami",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-		"X-RapidAPI-Host": "twitter60.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-const axios = require("axios");
-
-const options = {
-  method: 'GET',
-  url: 'https://twitter60.p.rapidapi.com/user_info',
-  params: {user_name: 'yua_mikami'},
-  headers: {
-    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    'X-RapidAPI-Host': 'twitter60.p.rapidapi.com'
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+  
+  function selectedArtist(hashtag) {
+  
+  
+  const options = {
+    method: 'GET',
+    url: 'https://twitter60.p.rapidapi.com/user_info',
+    params: {user_name: 'yua_mikami'},
+    headers: {
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'twitter60.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+      console.log(response.data);
+  }).catch(function (error) {
+      console.error(error);
+  });
+  } 
