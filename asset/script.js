f62941362b090d5d9b5d99d26bd30ead34bb9505
ShokoLocoMocco <stachikawa@gradcenter.cuny.edu>
2022-08-04 09:43:38 -0400

    tw

--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,32 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://twitter60.p.rapidapi.com/user_info?user_name=yua_mikami",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+		"X-RapidAPI-Host": "twitter60.p.rapidapi.com"
+	}
+};
 
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://twitter60.p.rapidapi.com/user_info',
+  params: {user_name: 'yua_mikami'},
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'twitter60.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
